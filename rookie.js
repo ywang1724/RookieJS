83a5d404b33b8016af12868342dd03761156d7a9
ywang1724 <kobebryant42536@gmail.com>
2014-12-03 16:57:33 +0800

    a few modifications

--- rookie.js
+++ rookie.js
@@ -1,26 +1,26 @@
 /**
  * Rookie.js v0.0.1
- * (c) 2014 wangyi
+ * Copyright (c) 2014 wangyi
  */
 (function (window) {
     'use strict';
 
     var timing = window.performance.timing;
     window.rookie = window.rookie || {
-
+        /*主文档监测*/
         navTiming: timing,
 
         getMainDocTimes: function () {
             var times = {};
 
             if (timing) {
-                /*网络耗时*/
+                //网络耗时
                 times.networkTime = timing.responseEnd - timing.fetchStart;
 
-                /*前端耗时*/
+                //前端耗时
                 times.frontendTime = timing.loadEventEnd - timing.responseEnd;
 
-                /*页面加载总耗时*/
+                //页面加载总耗时
                 times.totalTime = timing.loadEventEnd - timing.navigationStart;
             }
 
@@ -35,8 +35,20 @@
                 };
             });
             console.table(table);
-        }
+        },
 
+        /*资源监测*/
+        testBrowserCompatibility: function () {
+            /*Resource Timing*/
+            if (!('performance' in window) || !('getEntriesByType' in window.performance) ||
+                !(window.performance.getEntriesByType('resource') instanceof Array)) {
+                // API not supported
+                console.log('API not supported');
+            } else {
+                // API supported
+                console.log('API supported');
+            }
+        }
     };
 
     window.onload = function () {
