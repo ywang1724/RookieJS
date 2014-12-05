b959a6e8600caa8eb2d8afd73c935e2ddd107b38
ywang1724 <kobebryant42536@gmail.com>
2014-12-05 15:30:45 +0800

    improve the code

--- src/rookie.js
+++ src/rookie.js
@@ -26,15 +26,20 @@ window.onload = function () {
         rookie.errors.push("浏览器不支持Resource Timing API！");
     }
 
-    //检测页面是否加载完成
-    if (rookie.timings.loadEventEnd - rookie.timings.navigationStart < 0) {
-        rookie.errors.push("页面还在加载，获取数据失败！");
-    }
+    /*为什么此时页面还未完成加载？*/
+    ////检测页面是否加载完成
+    //if (rookie.timings.loadEventEnd - rookie.timings.navigationStart < 0) {
+    //    rookie.errors.push("页面还在加载，获取数据失败！");
+    //}
 
     /*Rookie工具包*/
     var rookieUtils = {
 
-        /*主文档监测*/
+        /**
+         * 主文档监测
+         * @param navTiming(PerformanceTiming Object)
+         * @returns {{}}
+         */
         getMainDocTimes: function (navTiming) {
             var mainDoc = {};
 
@@ -45,19 +50,66 @@ window.onload = function () {
             mainDoc.name = pathname.substring(lastIndex + 1);
 
             if (mainDoc) {
-                //网络耗时
-                mainDoc.networkTime = navTiming.responseEnd - navTiming.fetchStart;
-
-                //前端耗时
-                mainDoc.frontendTime = navTiming.loadEventEnd - navTiming.responseEnd;
-
-                //页面加载总耗时
-                mainDoc.totalTime = navTiming.loadEventEnd - navTiming.navigationStart;
+                mainDoc.networkTime = navTiming.responseEnd - navTiming.fetchStart;  //网络耗时
+                mainDoc.frontendTime = navTiming.loadEventEnd - navTiming.responseEnd;  //前端耗时
+                mainDoc.totalTime = navTiming.loadEventEnd - navTiming.navigationStart;  //页面加载总耗时
             }
 
             return mainDoc;
         },
 
+        /**
+         * 资源监测
+         * @param resTimings(PerformanceResourceTiming Object)
+         * @returns {Array}
+         */
+        getResourceTimes: function (resTimings) {
+            var resources = [];
+            for (var i = 0; i < resTimings.length; i++) {
+                var obj = {};
+                obj.totalTime = resTimings[i].duration;
+                obj.href = resTimings[i].name;
+                var lastIndex = resTimings[i].name.lastIndexOf('/'),
+                    index3 = resTimings[i].name.indexOf('/', 8);
+                obj.name = resTimings[i].name.substring(lastIndex + 1);
+                obj.path = resTimings[i].name.substring(index3 + 1, lastIndex);
+                resources.push(obj);
+            }
+            return resources;
+        },
+
+        /**
+         * 生成Waterfall数据
+         * @param navTiming(PerformanceTiming Object)
+         * @param resTimings(PerformanceResourceTiming Object)
+         * @returns {{}}
+         */
+        generateData: function (navTiming, resTimings) {
+            var data = {
+                name: [],
+                totalTime: []
+            };
+            var mainDoc = rookieUtils.getMainDocTimes(navTiming);
+            data.name.push(mainDoc.name);
+            data.totalTime.push(mainDoc.totalTime);
+            for (var i = 0; i < resTimings.length; i++) {
+                var obj = {};
+                obj.totalTime = resTimings[i].duration;
+                obj.href = resTimings[i].name;
+                var lastIndex = resTimings[i].name.lastIndexOf('/'),
+                    index3 = resTimings[i].name.indexOf('/', 8);
+                obj.name = resTimings[i].name.substring(lastIndex + 1);
+                obj.path = resTimings[i].name.substring(index3 + 1, lastIndex);
+                data.name.push(obj.name);
+                data.totalTime.push(obj.totalTime);
+            }
+            return data;
+        },
+
+        /**
+         * 控制台表格输出
+         * @param data(Object)
+         */
         printTable: function (data) {
             var table = {};
             Object.keys(data).sort().forEach(function (i) {
@@ -68,6 +120,10 @@ window.onload = function () {
             console.table(table);
         },
 
+        /**
+         * 瀑布流输出
+         * @param data
+         */
         drawWaterfall: function (data) {
             $('#container').highcharts({
                 chart: {
@@ -80,9 +136,9 @@ window.onload = function () {
                     text: 'Beta v0.0.1'
                 },
                 xAxis: {
-                    categories: [data.name],
+                    categories: data.name,
                     title: {
-                        text: null
+                        text: '加载文件'
                     }
                 },
                 yAxis: {
@@ -113,25 +169,28 @@ window.onload = function () {
                 },
                 series: [{
                     name: '总时间',
-                    data: [data.totalTime]
+                    data: data.totalTime
                 }]
             });
         }
-
-        /*资源监测*/
 
     };
 
     /*利用工具包分析页面加载数据调用（beta）*/
     setTimeout(function () {
+        //检测页面是否加载完成
+        if (rookie.timings.loadEventEnd - rookie.timings.navigationStart < 0) {
+            rookie.errors.push("页面还在加载，获取数据失败！");
+        }
         if (rookie.errors.length > 0) {
-            rookieUtils.printTable(rookieUtils.getMainDocTimes(rookie.timings));
-            rookieUtils.printTable(rookie.timings);
-            rookieUtils.drawWaterfall(rookieUtils.getMainDocTimes(rookie.timings));
-        } else {
             for (var item in rookie.errors) {
-                alert(item);
+                alert(rookie.errors[item]);
             }
+            rookieUtils.printTable(rookie.timings);
+        } else {
+            //rookieUtils.printTable(rookieUtils.getMainDocTimes(rookie.timings));
+            //rookieUtils.printTable(rookie.timings);
+            rookieUtils.drawWaterfall(rookieUtils.generateData(rookie.timings, rookie.resources));
         }
     }, 0);
 
