d0da852cc4d5916cf87bacd469f56ebaa2c9f519
ywang1724 <kobebryant42536@gmail.com>
2014-12-15 10:19:02 +0800

    use drilldown

--- src/rookie.js
+++ src/rookie.js
@@ -49,10 +49,20 @@ window.onload = function () {
             mainDoc.path = pathname.substring(0, lastIndex);
             mainDoc.name = pathname.substring(lastIndex + 1);
 
+            var start = navTiming.navigationStart;
             if (mainDoc) {
                 mainDoc.networkTime = navTiming.responseEnd - navTiming.fetchStart;  //网络耗时
                 mainDoc.frontendTime = navTiming.loadEventEnd - navTiming.responseEnd;  //前端耗时
                 mainDoc.totalTime = navTiming.loadEventEnd - navTiming.navigationStart;  //页面加载总耗时
+                mainDoc.dnsTime = navTiming.domainLookupEnd - navTiming.domainLookupStart;
+                mainDoc.connectionTime = navTiming.connectEnd - navTiming.connectStart;
+                mainDoc.ttfbTime = navTiming.responseStart - navTiming.navigationStart;
+                mainDoc.transferTime = navTiming.responseEnd - navTiming.responseStart;
+
+                mainDoc.dns = [navTiming.domainLookupStart - start, navTiming.domainLookupEnd - start];
+                mainDoc.connection = [navTiming.connectStart - start, navTiming.connectEnd - start];
+                mainDoc.ttfb = [0, navTiming.responseStart - start];
+                mainDoc.transfer = [navTiming.responseStart - start, navTiming.responseEnd - start];
             }
 
             return mainDoc;
@@ -74,6 +84,12 @@ window.onload = function () {
                 obj.name = resTimings[i].name.substring(lastIndex + 1);
                 obj.path = resTimings[i].name.substring(index3 + 1, lastIndex);
                 obj.startTime = resTimings[i].startTime;
+
+                obj.dns = [Math.round(resTimings[i].domainLookupStart), Math.round(resTimings[i].domainLookupEnd)];
+                obj.connection = [Math.round(resTimings[i].connectStart), Math.round(resTimings[i].connectEnd)];
+                obj.ttfb = [Math.round(resTimings[i].startTime), Math.round(resTimings[i].responseStart)];
+                obj.transfer = [Math.round(resTimings[i].responseStart), Math.round(resTimings[i].responseEnd)];
+
                 resources.push(obj);
             }
             return resources;
@@ -89,12 +105,36 @@ window.onload = function () {
             var data = {
                 name: [],
                 totalTime: [],
-                timeline: []
+                timeline: [],
+                series: [],
+                drilldownSeries: []
             };
+
+            function CRDataObj(name, low, high, drilldown) {
+                this.name = name || null;
+                this.low = low || 0;
+                this.high = high || 0;
+                this.drilldown = drilldown || "";
+            }
+
+            function CRSeriesObj(id, name, data) {
+                this.id = id || "";
+                this.name = name || null;
+                this.data = data || [];
+            }
+
             var mainDoc = rookieUtils.getMainDocTimes(navTiming);
             data.name.push(mainDoc.name);
             data.totalTime.push(mainDoc.totalTime);
             data.timeline.push([0, mainDoc.totalTime]);
+            var seriesObj = new CRSeriesObj("", "时间线");
+            seriesObj.data.push(new CRDataObj(mainDoc.name, 0, mainDoc.totalTime, mainDoc.name));
+            var ddSeriesObjM = new CRSeriesObj(mainDoc.name);
+            ddSeriesObjM.data.push(new CRDataObj("dns", mainDoc.dns[0], mainDoc.dns[1]));
+            ddSeriesObjM.data.push(new CRDataObj("connection", mainDoc.connection[0], mainDoc.connection[1]));
+            ddSeriesObjM.data.push(new CRDataObj("ttfb", mainDoc.ttfb[0], mainDoc.ttfb[1]));
+            ddSeriesObjM.data.push(new CRDataObj("transfer", mainDoc.transfer[0], mainDoc.transfer[1]));
+            data.drilldownSeries.push(ddSeriesObjM);
             for (var i = 0; i < resTimings.length; i++) {
                 var obj = {};
                 obj.totalTime = resTimings[i].duration;
@@ -106,8 +146,25 @@ window.onload = function () {
                 data.name.push(obj.name);
                 data.totalTime.push(Math.round(obj.totalTime));
                 data.timeline.push([Math.round(resTimings[i].startTime), Math.round(resTimings[i].startTime + obj.totalTime)]);
-            }
+                var low = Math.round(resTimings[i].startTime),
+                    high = Math.round(resTimings[i].startTime + obj.totalTime);
+                seriesObj.data.push(new CRDataObj(obj.name, low, high, obj.name));
 
+                obj.dns = [Math.round(resTimings[i].domainLookupStart), Math.round(resTimings[i].domainLookupEnd)];
+                obj.connection = [Math.round(resTimings[i].connectStart), Math.round(resTimings[i].connectEnd)];
+                obj.ttfb = [Math.round(resTimings[i].startTime), Math.round(resTimings[i].responseStart)];
+                obj.transfer = [Math.round(resTimings[i].responseStart), Math.round(resTimings[i].responseEnd)];
+
+                var ddSeriesObj = new CRSeriesObj(obj.name);
+                ddSeriesObj.data.push(new CRDataObj("dns", obj.dns[0], obj.dns[1]));
+                ddSeriesObj.data.push(new CRDataObj("connection", obj.connection[0], obj.connection[1]));
+                ddSeriesObj.data.push(new CRDataObj("ttfb", obj.ttfb[0], obj.ttfb[1]));
+                ddSeriesObj.data.push(new CRDataObj("transfer", obj.transfer[0], obj.transfer[1]));
+                data.drilldownSeries.push(ddSeriesObj);
+            }
+            data.series.push(seriesObj);
+            console.log(JSON.stringify(data.series));
+            console.log(JSON.stringify(data.drilldownSeries));
             return data;
         },
 
@@ -199,7 +256,7 @@ window.onload = function () {
                     text: 'Beta v0.0.1'
                 },
                 xAxis: {
-                    categories: data.name
+                    type: 'category'
                 },
                 yAxis: {
                     min: 0,
@@ -210,7 +267,7 @@ window.onload = function () {
                 },
                 tooltip: {
                     formatter: function () {
-                        return '<h6>' + this.x + '</h6><br/><table><tr><td>总耗时: </td><td><b>' +
+                        return '<h6>' + this.key + '</h6><br/><table><tr><td>总耗时: </td><td><b>' +
                             (this.point.high - this.point.low) + ' ms' + '</b></td></tr></table>';
                     }
                 },
@@ -230,10 +287,10 @@ window.onload = function () {
                 credits: {
                     enabled: false
                 },
-                series: [{
-                    name: '时间线',
-                    data: data.timeline
-                }]
+                series: data.series,
+                drilldown: {
+                    series: data.drilldownSeries
+                }
             });
         }
 
